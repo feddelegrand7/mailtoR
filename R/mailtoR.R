144d961dbd4b89e7c10a3bcdfcd09f0d9d1694f3
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2020-06-19 04:01:53 +0200

    functions documented

--- R/mailtoR.R
+++ R/mailtoR.R
@@ -1,10 +1,49 @@
 
+#' Create a friendly user interface for sending emails
+#'
+#' @param email the emails of the recipients
+#' @param text the link text that will be displayed on the ui
+#' @param subject the subject of the email. Defaults to NULL
+#' @param cc the emails of the CC (carbon copy) recipients. Defaults to NULL
+#' @param bcc the emails of the BCC (blind carbon copy) recipients. Defaults to NULL
+#' @param body the body of the email. Defaults to NULL
+#'
+#' @return a user interface for sending emails
+#' @export
+#'
+#' @examples
+#'
+#'
+#' if (interactive()) {
+#'
+#'
+#'
+#' ui <- fluidPage(
+#'
+#'     mailtoR(email = "",
+#'     text = "click here to send an email"),
+#'     use_mailtoR()
+#'
+#'     )
+#'
+#'server <- function(input, output){
+#'
+#'
+#'}
+#'
+#'
+#'shinyApp(ui, server)
+#'
+#'
+#'
+#' }
+
+
+
+
+
 mailtoR <- function(email, text, subject = NULL, cc = NULL, bcc = NULL, body = NULL){
 
-
-
-
-  # <a class="mailtoui" href="mailto:user1@example.com?subject=Hi%20there!">With subject</a>
 
 
 
@@ -24,7 +63,6 @@ mailtoR <- function(email, text, subject = NULL, cc = NULL, bcc = NULL, body = N
   } else if (is.null(subject) & !is.null(cc) & is.null(bcc) & is.null(body)) {
 
 
-    #<a class="mailtoui" href="mailto:user2@example.com?cc=user3@example.com">With cc</a>
 
     htmltools::a(class = "mailtoui", href = glue::glue("mailto:{toString(email)}?cc={toString(cc)}"), glue::glue("{text}"))
 
@@ -32,7 +70,6 @@ mailtoR <- function(email, text, subject = NULL, cc = NULL, bcc = NULL, body = N
   }else if (is.null(subject) & !is.null(cc) & !is.null(bcc) & is.null(body)) {
 
 
-  # <a class="mailtoui" href="mailto:user3@example.com?bcc=user1@example.com,user5@example.com">With multiple bcc</a>
 
     htmltools::a(class = "mailtoui", href = glue::glue("mailto:{toString(email)}?bcc={toString(bcc)}"), glue::glue("{text}"))
 
@@ -44,7 +81,6 @@ mailtoR <- function(email, text, subject = NULL, cc = NULL, bcc = NULL, body = N
   body <- stringr::str_replace_all(string = body, pattern = " ", replacement = "%20")
 
 
-    # <a class="mailtoui" href="mailto:user4@example.com?body=Hope%20you're%20doing%20well.">With body</a>
 
     htmltools::a(class = "mailtoui", href = glue::glue("mailto:{toString(email)}?body={toString(body)}"), glue::glue("{text}"))
 
@@ -54,24 +90,15 @@ mailtoR <- function(email, text, subject = NULL, cc = NULL, bcc = NULL, body = N
   } else {
 
     body <- stringr::str_replace_all(string = body, pattern = " ", replacement = "%20")
+
     subject <- stringr::str_replace_all(string = subject, pattern = " ", replacement = "%20")
 
     href <- glue::glue("mailto:{toString(email)}?subject={toString(subject)}&cc={toString(cc)}&bcc={toString(bcc)}&body={toString(body)}")
+
     htmltools::a(class = "mailtoui", href = href, glue::glue("{text}"))
 
 
-    # <a class="mailtoui" href="mailto:user5@example.com,user2@example.com?subject=Winter%20is%20coming&cc=user3@example.com,user4@example.com&bcc=user1@example.com&body=Fear%20cuts%20deeper%20than%20swords.">With all fields</a>
-
-
-
-
-
   }
-
-
-
-
-
 
 
 }
--- R/use_mailtoR.R
+++ R/use_mailtoR.R
@@ -1,5 +1,23 @@
 
 
+#' Enable the MailtoUI javascript library
+#' @description The function activates the capabilities of the MailtoUI javascript library. The user needs to set it at the end of the shiny ui.
+#'
+#' @return called for the side effect of activating the MailtoUI library
+#' @export
+#'
+#' @examples \donttest{
+#'
+#' # Put the function at the bottom of the ui
+#'
+#' use_mailtoR()
+#'
+#'
+#'
+#' }
+
+
+
 use_mailtoR <- function(){
 
 
